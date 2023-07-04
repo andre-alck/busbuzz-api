f98797fdb630119caa99e1473d924b276d1cc68f
andre-alck <andrealck1@gmail.com>
2023-07-04 01:06:15 -0300

    build: init spring boot project using spring inititializr

---
+++ busbuzz-api/src/main/java/asac/com/br/busbuzzapi/BusbuzzApiApplication.java
@@ -0,0 +1,13 @@
+package asac.com.br.busbuzzapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbuzzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusbuzzApiApplication.class, args);
+	}
+
+}
