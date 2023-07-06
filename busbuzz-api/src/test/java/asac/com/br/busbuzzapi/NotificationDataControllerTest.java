184aca291a2544f2a0b4e947042d7d27ed4f97d6
andre-alck <andrealck1@gmail.com>
2023-07-06 18:59:55 -0300

    test: validate imageUrl and text from NotificationDataModel

--- busbuzz-api/src/test/java/asac/com/br/busbuzzapi/NotificationDataControllerTest.java
+++ busbuzz-api/src/test/java/asac/com/br/busbuzzapi/NotificationDataControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -35,6 +36,43 @@ class NotificationDataControllerTest {
     @Test
     void givenInvalidImageUrlThenShouldReturnAssertionError() throws JsonProcessingException {
         NotificationDataModel model = new NotificationDataModel("title", "text", "imageUrl", "name");
+        String modelJson = objectMapper.writeValueAsString(model);
+
+        AssertionError exception = assertThrows(
+                AssertionError.class, () -> {
+                    this.mockMvc.perform(post("/add-notification-data")
+                                    .contentType("application/json")
+                                    .content(modelJson))
+                            .andDo(print())
+                            .andExpect(status().isOk())
+                            .andExpect(content().string(containsString("hello, world!")));
+                }
+        );
+
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Status expected:<200> but was:<400>";
+        assertThat(actualMessage).isEqualTo(expectedMessage);
+    }
+
+    @Test
+    void givenNullImageUrlThenShouldReturnOk() throws Exception {
+        NotificationDataModel model = new NotificationDataModel("title", "text", null, "name");
+        String modelJson = objectMapper.writeValueAsString(model);
+
+        this.mockMvc.perform(post("/add-notification-data").contentType("application/json").content(modelJson)).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("hello, world!")));
+    }
+
+    @Test
+    void givenValidImageUrlThenShouldReturnOk() throws Exception {
+        NotificationDataModel model = new NotificationDataModel("title", "text", "https://avatars.githubusercontent.com/u/60830711?s=400&u=b201e170a816d600b000cde8a2135101fff0ec7f&v=4", "name");
+        String modelJson = objectMapper.writeValueAsString(model);
+
+        this.mockMvc.perform(post("/add-notification-data").contentType("application/json").content(modelJson)).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("hello, world!")));
+    }
+
+    @Test
+    void givenNullTextThenShouldThrowAlgo() throws Exception {
+        NotificationDataModel model = new NotificationDataModel("title", null, "https://avatars.githubusercontent.com/u/60830711?s=400&u=b201e170a816d600b000cde8a2135101fff0ec7f&v=4", "name");
         String modelJson = objectMapper.writeValueAsString(model);
 
         AssertionError exception = assertThrows(
